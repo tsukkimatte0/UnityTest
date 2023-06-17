557a7bc220e84bec2b3249a1d772bdedc4808187
tsukkimatte0 <iammatz@gmail.com>
2023-06-17 13:21:41 +0200

    SecondRepo

---
+++ New Github Test/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private float horizontalInput;
+    private float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        transform.Translate(Vector3.forward * verticalInput * speed * Time.deltaTime);
+    }
+}
+
